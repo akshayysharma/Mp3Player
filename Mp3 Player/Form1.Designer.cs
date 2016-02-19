ed9782c09f748d5f4f1453d6159743c3bb1e5636
Akshay Sharma <akshay.sharma76757@gmail.com>
2016-02-19 19:55:05 +0530

    first commit

---
+++ Mp3 Player/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Mp3_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.btnPlayAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(245, 46);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "Load Files";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // btnPlayAll
+            // 
+            this.btnPlayAll.Location = new System.Drawing.Point(12, 500);
+            this.btnPlayAll.Name = "btnPlayAll";
+            this.btnPlayAll.Size = new System.Drawing.Size(245, 49);
+            this.btnPlayAll.TabIndex = 1;
+            this.btnPlayAll.Text = "Play All";
+            this.btnPlayAll.UseVisualStyleBackColor = true;
+            this.btnPlayAll.Click += new System.EventHandler(this.btnPlayAll_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 75);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(245, 419);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 116;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Location";
+            this.columnHeader2.Width = 166;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(263, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(509, 537);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnPlayAll);
+            this.Controls.Add(this.btnLoadFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MP3 Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Button btnPlayAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Mp3 Player/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Mp3_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            Stream loadfile = null;
+            OpenFileDialog open = new OpenFileDialog();
+            
+            //set up the properties like initial directories,types of files etc.
+            open.InitialDirectory = "c:\\";
+            
+            //type of files which we want
+            open.Filter = "MP3 Audio file (*.mp3)|*.mp3|Windows Media Audio File (*.wma)|*.wma|WAV Audio File (*.wav)|*.wav|All Files (*.*)|*.*";
+            
+            //filter index=filter which we want to display by default
+            //ex in my code mp3 is 1 in our filter so set it to 1 
+            //setting it 2 it would display wma files first
+            open.FilterIndex = 1;
+            
+            //Restore directory=it will restore the default directory every time we brouse the new files
+            open.RestoreDirectory = false;
+
+            //multiselect=allow us to select multiple files
+            open.Multiselect = true;
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((loadfile = open.OpenFile()) != null)
+                    {
+                        using (loadfile)
+                        {
+                            //setting up the strings
+                            //setting up the absolute file path and the safe file path
+                            string[] fileNameAndPath = open.FileNames;
+                            string[] filename = open.SafeFileNames;
+
+                            for (int i = 0; i < open.SafeFileName.Count(); i++)
+                            {
+                                //adding each file to the list view
+                                //str[0]=main file or the safe file name
+                                //str[1]=secontd main file or the absolute path used by media player
+                                string[] str = new string[2];
+                                str[0] = filename[i];
+                                str[1] = fileNameAndPath[i];
+
+                                ListViewItem lvi = new ListViewItem(str);
+                                listView1.Items.Add(lvi);
+                            }
+                        }
+                    }
+                }
+                    //if file not found
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:Could not read the file from the disk");
+                }
+            }
+        }
+
+        private void btnPlayAll_Click(object sender, EventArgs e)
+        {
+            WMPLib.IWMPPlaylist play = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");
+            WMPLib.IWMPMedia media;
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                int j = 1;
+                
+                //add subitems to the playlist
+                //playlist=basic text file contain absolute path to the rext files
+                media = axWindowsMediaPlayer1.newMedia(listView1.Items[i].SubItems[j].Text);
+                play.appendItem(media);
+                j++;
+
+                //set the media player current playlist to the one which we have created
+                axWindowsMediaPlayer1.currentPlaylist = play;
+                
+                //play the files
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+
+            }
+            
+        }
+
+        //getting the single selected files
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            //play the selected files
+            string select = listView1.FocusedItem.SubItems[1].Text;
+            axWindowsMediaPlayer1.URL = @select;
+        }
+
+      
+
+       
+    }
+}
